f76bc890ee9083dbad6674d42451fb52edf0a257
Denny <denny.george90@gmail.com>
2024-04-07 14:49:17 +0530

    refactor: search param test

---
+++ test/dau_web/live/search/search_live_test.exs
@@ -0,0 +1,14 @@
+defmodule DAUWeb.Search.SearchLiveTest do
+  use DAUWeb.ConnCase, async: true
+
+  # import Phoenix.LiveViewTest
+
+  describe "search filtering" do
+    # test "filter by media type", %{conn: conn} do
+    #   {:ok, view, html} = live(conn, "/demo/query")
+    #   assert html =~ "<h1>Showing</h1>"
+    #   IO.inspect(view)
+    #   IO.inspect(html)
+    # end
+  end
+end
