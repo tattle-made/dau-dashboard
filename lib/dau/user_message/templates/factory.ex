588735aff90e64641b71b5bc19087e9c8a0efb4c
Denny <denny.george90@gmail.com>
2024-04-10 21:38:10 +0530

    feat: template factory and transformation

---
+++ lib/dau/user_message/templates/factory.ex
@@ -0,0 +1,53 @@
+defmodule DAU.UserMessage.Templates.Factory do
+  @moduledoc """
+  Map data added to feed_common to appropriate response template
+
+  Responses send to users are standardized using templates. This serves two purposes - reduce burden on the editors
+  in composing a response to the user and it adheres to how whatsapp allows sending responses to users after 24 hours.
+
+  What response is sent to a user depends on the following :
+  1. Is there an assessment report attached by the DAU secratariat
+  2. Are there factcheck articles attached to the query and how many?
+  3. Verification status of the query
+
+  ## Example
+  alias DAU.Repo
+  alias DAU.Feed.Common
+  alias DAU.UserMessage.Templates.Factory
+
+  common = Repo.get(Common, 1)
+  template = Factory.process(common)
+  text = Factory.eval(template)
+  "📢 We reviewed this audio/video and found it to be a Deepfake.\n\n🎯You can read our assessment report here: https://report-url\n\n🧠 Please use your discretion in sharing this information.\n\nThank you for reaching out to us. We hope you have a good day ahead. 🙏"
+  """
+  alias DAU.UserMessage.Templates.Template
+  alias DAU.Feed.Common
+  require EEx
+
+  @base_path "lib/dau/user_message/templates"
+
+  def process(%Common{} = common) do
+    common
+    |> Template.change()
+    |> Template.assign_assessment_report()
+    |> Template.assign_factcheck_articles()
+    |> Template.assign_verification_status()
+    |> Template.assign_template_parameters()
+    |> Template.assign_template_name()
+    |> Template.assign_valid()
+  end
+
+  def eval(%Template{} = template) do
+    try do
+      text =
+        EEx.eval_file(
+          "#{@base_path}/#{template.meta.template_name}.eex",
+          template.meta.template_parameters
+        )
+
+      {:ok, String.trim(text)}
+    rescue
+      File.Error -> {:error, "There is no template file for this configuration."}
+    end
+  end
+end
---
+++ lib/dau/user_message/templates/template.ex
@@ -0,0 +1,116 @@
+defmodule DAU.UserMessage.Templates.Template do
+  alias DAU.UserMessage.Templates.Template
+  alias DAU.Feed.Common
+  defstruct [:data, :meta, :function, :template_name]
+
+  def change(%Common{} = common) do
+    %Template{}
+    |> Map.put(:data, common)
+    |> Map.put(:meta, %{})
+    |> Map.put(:function, nil)
+  end
+
+  def assign_assessment_report(%Template{} = template) do
+    data = template.data
+    value = Map.get(data, :assessment_report, nil)
+    new_meta = Map.put(template.meta, :assessment_report, value)
+    Map.put(template, :meta, new_meta)
+  end
+
+  def assign_factcheck_articles(%Template{} = template) do
+    data = template.data
+    factcheck_articles = Map.get(data, :factcheck_articles, [])
+
+    value =
+      Enum.map(factcheck_articles, fn article ->
+        %{
+          domain: "TODO",
+          url: article.url
+        }
+      end)
+
+    new_meta = Map.put(template.meta, :factcheck_articles, value)
+
+    Map.put(template, :meta, new_meta)
+  end
+
+  def assign_verification_status(%Template{} = template) do
+    data = template.data
+    value = Map.get(data, :verification_status, nil)
+    new_meta = Map.put(template.meta, :verification_status, value)
+    Map.put(template, :meta, new_meta)
+  end
+
+  def assign_template_name(%Template{} = template) do
+    name_parts = [
+      template_name_label(:verification_status, template),
+      template_name_label(:assessment_report, template),
+      template_name_label(:factcheck_articles, template)
+    ]
+
+    name = Enum.reduce(name_parts, "", fn acc, name -> "#{name}_#{acc}" end)
+    name = name |> String.slice(1..-1)
+
+    new_meta = Map.put(template.meta, :template_name, name)
+    Map.put(template, :meta, new_meta)
+  end
+
+  defp template_name_label(:verification_status, %Template{meta: meta}) do
+    Atom.to_string(meta.verification_status)
+  end
+
+  defp template_name_label(:factcheck_articles, %Template{meta: meta}) do
+    case length(meta.factcheck_articles) do
+      0 -> "0fc"
+      1 -> "1fc"
+      2 -> "2fc"
+      _ -> raise "Too many fact check articles"
+    end
+  end
+
+  defp template_name_label(:assessment_report, %Template{meta: meta}) do
+    case meta.assessment_report do
+      nil -> "wo_ar"
+      _ -> "w_ar"
+    end
+  end
+
+  def assign_valid(%Template{meta: %{template_name: template_name}} = template) do
+    is_valid =
+      Enum.member?(
+        [
+          "deepfake_w_ar_0fc",
+          "deepfake_wo_ar_2fc",
+          "manipulated_wo_ar_2fc",
+          "manipulated_w_ar_0fc",
+          "not_manipulated_wo_ar_0fc",
+          "not_manipualted_wo_ar_2fc",
+          "inconclusive_w_ar_0fc",
+          "out_of_scope",
+          "not_ai_gen_wo_ar_0fc",
+          "not_ai_gen_wo_ar_2fc"
+
+          # "deepfake_wo_ar_1fc",
+          # "not_manipulated_wo_ar_1fc",
+          # "not_ai_gen_wo_ar_1fc",
+          # "unsupported_language_wo_ar_0fc",
+          # "ai_generated_wo_ar_0fc",
+          # "ai_generated_w_ar_0fc",
+          # "ai_generated_wo_ar_1fc",
+          # "ai_generated_wo_ar_2fc",
+          # "spam_wo_ar_0fc"
+        ],
+        template_name
+      )
+
+    new_meta = Map.put(template.meta, :valid, is_valid)
+    Map.put(template, :meta, new_meta)
+  end
+
+  def assign_template_parameters(%Template{meta: meta} = template) do
+    map_to_keywords = Enum.map(meta, fn {k, v} -> {k, v} end)
+
+    new_meta = Map.put(template.meta, :template_parameters, map_to_keywords)
+    Map.put(template, :meta, new_meta)
+  end
+end
