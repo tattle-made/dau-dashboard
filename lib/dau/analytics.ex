cacdf5694b3ef6f42073ce04d735dafe3f285ace
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-05-03 16:15:53 +0530

    feat: counting urls and displaying data on a table

--- lib/dau/analytics.ex
+++ lib/dau/analytics.ex
@@ -3,12 +3,20 @@ defmodule DAU.Analytics do
   alias DAU.Feed.Common
   alias DAU.Repo
 
-  def fetch_author_and_url do
-    Common
-    |> query_author_and_url()
-    |> limit(10)
-    |> offset(0)
+  def fetch_author_and_url(opts \\ []) do
+    query =
+      Common
+      |> query_author_and_url()
+
+    query =
+      case Keyword.get(opts, :test_run) do
+        true -> query |> limit(20) |> offset(0)
+        _ -> query
+      end
+
+    query
     |> Repo.all()
+    |> count_urls_per_username()
   end
 
   defp query_author_and_url(query) do
@@ -17,5 +25,12 @@ defmodule DAU.Analytics do
         username: fragment("jsonb_array_elements(?)->>'username'", c.factcheck_articles),
         url: fragment("jsonb_array_elements(?)->>'url'", c.factcheck_articles)
       }
+  end
+
+  defp count_urls_per_username(data) do
+    data
+    |> Enum.group_by(& &1.username)
+    |> Enum.map(fn {username, urls} -> %{username: username, url_count: Enum.count(urls)} end)
+    |> Enum.sort(&(&1.url_count >= &2.url_count))
   end
 end
--- lib/dau_web/controllers/analytics_html.ex
+++ lib/dau_web/controllers/analytics_html.ex
@@ -1,14 +1,17 @@
 defmodule DAUWeb.AnalyticsHTML do
   use DAUWeb, :html
+  import DAUWeb.CoreComponents
 
   def index(assigns) do
     ~H"""
-    <h1>HELLO</h1>
-    <ul>
-      <%= for item <- @data do %>
-        <li><%= item.url %></li>
-      <% end %>
-    </ul>
+    <div class="flex justify-center">
+      <div class="w-full max-w-4xl mx-auto">
+        <.table id="analytics-table" rows={@data}>
+          <:col :let={item} label="Username"><%= item.username %></:col>
+          <:col :let={item} label="Factcheck Articles Added"><%= item.url_count %></:col>
+        </.table>
+      </div>
+    </div>
     """
   end
 end
