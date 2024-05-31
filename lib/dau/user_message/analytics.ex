0e25395902fe431e1e40f87bdee520eb65de99d0
dennyabrain <denny.george90@gmail.com>
2024-05-31 14:15:12 +0530

    dmp

---
+++ lib/dau/user_message/analytics.ex
@@ -0,0 +1,12 @@
+defmodule DAU.UserMessage.Analytics do
+  alias DAU.UserMessage.Analytics.Event
+
+  def get_all_chronologically() do
+  end
+
+  def get_all_for_feed_common(common_id) do
+  end
+
+  def save_event(%Event{} = event) do
+  end
+end
