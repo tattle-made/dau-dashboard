c87f41e24940fe90b363864288b654be9c1a39e2
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-06-04 21:41:34 +0530

    feat: auto tag incoming data as spam (#25)
    When a new message is received on the tipline, if a matching message is found and its verification status is spam, the new incoming message is also marked as spam.

--- lib/dau/feed.ex
+++ lib/dau/feed.ex
@@ -335,4 +335,9 @@ defmodule DAU.Feed do
     template_common = Factory.process(common)
     Template.valid?(template_common)
   end
+
+  def change_verification_status_to_spam(common_id) do
+    common_b = Repo.get!(Common, common_id)
+    common_b |> Common.changeset(%{verification_status: :spam}) |> Repo.update()
+  end
 end
