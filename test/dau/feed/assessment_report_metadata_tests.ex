9c12f26a9c79ad969804b596ef5aae85bd76d94b
Aatman Vaidya <aatmanvaidya@gmail.com>
2024-06-26 21:12:35 +0530

    test: CRUD test for assessment report metadata

--- test/dau/feed/assessment_report_metadata_tests.ex
+++ test/dau/feed/assessment_report_metadata_tests.ex
@@ -1,6 +1,7 @@
 defmodule DAU.Feed.AssessmentReportMetadataTest do
   alias DAU.Feed.Common
-  alias DAU.Feed.AssessmentReport
+  alias DAU.Feed.AssessmentReportMetadata
+  alias DAU.Repo
   use DAU.DataCase
 
   describe "CRUD for assessment report metadata" do
@@ -19,19 +20,110 @@ defmodule DAU.Feed.AssessmentReportMetadataTest do
     end
 
     test "create form entry", state do
-      IO.inspect(state.feed_common_id)
-
       attrs = %{
         link_of_ar: "https://example.com/assessment-report",
-        who_is_post_targeting: "General public",
+        who_is_post_targeting: "someone",
         language: "en",
         feed_common_id: state.feed_common_id
       }
 
       assessment_report_meta_entry =
-        %DAU.Feed.AssessmentReportMetadata{}
-        |> DAU.Feed.AssessmentReportMetadata.changeset(attrs)
+        %AssessmentReportMetadata{}
+        |> AssessmentReportMetadata.changeset(attrs)
         |> Repo.insert!()
+
+      assert assessment_report_meta_entry.link_of_ar == "https://example.com/assessment-report"
+      assert assessment_report_meta_entry.who_is_post_targeting == "someone"
+      assert assessment_report_meta_entry.language == "en"
+      assert assessment_report_meta_entry.feed_common_id == state.feed_common_id
+    end
+
+    test "fetch assessment report metadata by feed_common_id", state do
+      # Add data to assessment report metadata table
+      attrs = %{
+        link_of_ar: "https://example.com/assessment-report",
+        who_is_post_targeting: "someone",
+        language: "hi",
+        feed_common_id: state.feed_common_id
+      }
+
+      assessment_report_meta_entry =
+        %AssessmentReportMetadata{}
+        |> AssessmentReportMetadata.changeset(attrs)
+        |> Repo.insert!()
+
+      # Fetch based on feed_common_id
+      fetched_entry = Repo.get_by!(AssessmentReportMetadata, feed_common_id: state.feed_common_id)
+
+      assert fetched_entry.link_of_ar == "https://example.com/assessment-report"
+      assert fetched_entry.who_is_post_targeting == "someone"
+      assert fetched_entry.language == "hi"
+      assert fetched_entry.feed_common_id == state.feed_common_id
+    end
+
+    test "update values of assessment report metadata", state do
+      # create an exisiting record
+      attrs = %{
+        link_of_ar: "https://example.com/assessment-report",
+        who_is_post_targeting: "someone",
+        language: "en",
+        feed_common_id: state.feed_common_id
+      }
+
+      assessment_report_meta_entry =
+        %AssessmentReportMetadata{}
+        |> AssessmentReportMetadata.changeset(attrs)
+        |> Repo.insert!()
+
+      # update the record
+      updated_attrs = %{
+        link_of_ar: "https://example.com/assessment-report",
+        who_is_post_targeting: "none",
+        language: "ta",
+        feed_common_id: state.feed_common_id
+      }
+
+      updated_entry =
+        AssessmentReportMetadata
+        |> Repo.get_by!(feed_common_id: state.feed_common_id)
+        |> AssessmentReportMetadata.changeset(updated_attrs)
+        |> Repo.update!()
+
+      assert updated_entry.link_of_ar == "https://example.com/assessment-report"
+      assert updated_entry.who_is_post_targeting == "none"
+      assert updated_entry.language == "ta"
+      assert updated_entry.feed_common_id == state.feed_common_id
+    end
+
+    test "delete a entry in assessment report metadata", state do
+      # create an exisiting record
+      attrs = %{
+        link_of_ar: "https://example.com/assessment-report",
+        who_is_post_targeting: "someone",
+        language: "en",
+        feed_common_id: state.feed_common_id
+      }
+
+      assessment_report_meta_entry =
+        %AssessmentReportMetadata{}
+        |> AssessmentReportMetadata.changeset(attrs)
+        |> Repo.insert!()
+
+      # delete it - takes entire changeset
+      deleted_entry =
+        AssessmentReportMetadata
+        |> Repo.get_by(feed_common_id: state.feed_common_id)
+        |> Repo.delete!()
+
+      assert_raise Ecto.NoResultsError, fn ->
+        AssessmentReportMetadata
+        |> Repo.get_by!(feed_common_id: state.feed_common_id)
+      end
+
+      assert_raise Ecto.NoResultsError, fn ->
+        AssessmentReportMetadata
+        |> Repo.get!(deleted_entry.id)
+      end
     end
   end
 end
