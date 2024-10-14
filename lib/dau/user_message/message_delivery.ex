da631a2b2dfe47be89be4dff8eeecb29f9eefbaf
maanasb01 <maanasb01@gmail.com>
2024-10-14 19:53:36 +0530

    send basic hardcoded template message

--- lib/dau/user_message/message_delivery.ex
+++ lib/dau/user_message/message_delivery.ex
@@ -33,28 +33,67 @@ defmodule DAU.UserMessage.MessageDelivery.Production do
     HTTPoison.post(gupshup_endpoint, body, headers)
   end
 
-  def send_template_to_bsp(id, phone_number, message) do
+  # def send_template_to_bsp(id, phone_number, message) do
+  #   headers = [
+  #     {"Content-Type", "application/x-www-form-urlencoded"}
+  #   ]
+
+  #   bsp_user_id = Application.get_env(:dau, :gupshup_hsm_userid)
+  #   bsp_password = Application.get_env(:dau, :gupshup_hsm_password)
+  #   gupshup_api_endpoint = Application.get_env(:dau, :gupshup_api_endpoint)
+
+  #   params = %{
+  #     userid: bsp_user_id,
+  #     password: bsp_password,
+  #     send_to: phone_number,
+  #     v: "1.1",
+  #     format: "json",
+  #     msg_type: "TEXT",
+  #     method: "SENDMESSAGE",
+  #     msg: message,
+  #     msg_id: id
+  #   }
+
+  #   HTTPoison.post(gupshup_api_endpoint, "", headers, params: params)
+  # end
+
+  def send_template_to_bsp(phone_number, message) do
+    # turn_token = Application.get_env(:dau, :turn_token)
+    turn_token = System.get_env("TURN_TOKEN")
     headers = [
-      {"Content-Type", "application/x-www-form-urlencoded"}
+      {"Content-Type", "application/json"},
+      {"Authorization", "Bearer #{turn_token}"}
+
     ]
-
-    bsp_user_id = Application.get_env(:dau, :gupshup_hsm_userid)
-    bsp_password = Application.get_env(:dau, :gupshup_hsm_password)
-    gupshup_api_endpoint = Application.get_env(:dau, :gupshup_api_endpoint)
-
-    params = %{
-      userid: bsp_user_id,
-      password: bsp_password,
-      send_to: phone_number,
-      v: "1.1",
-      format: "json",
-      msg_type: "TEXT",
-      method: "SENDMESSAGE",
-      msg: message,
-      msg_id: id
+#DAU.UserMessage.MessageDelivery.Production.send_template_to_bsp("919999999999","test-test")
+    body = """
+    {
+        "to": "#{phone_number}",
+        "type": "template",
+        "template": {
+            "namespace": "0863995f_325c_4cd7_a1b5_0c488c30311f",
+            "name": "cheapfake_w_ar_0fc_en",
+            "language": {
+                "code": "en",
+                "policy": "deterministic"
+            },
+            "components": [
+                {
+                    "type": "body",
+                    "parameters": [
+                        {
+                            "type": "text",
+                            "text": "https://www.dau.mcaindia.in/blog/video-of-sudha-murty-promoting-a-financial-program-is-fake"
+                        }
+                    ]
+                }
+            ]
+        }
     }
+"""
 
-    HTTPoison.post(gupshup_api_endpoint, "", headers, params: params)
+
+    HTTPoison.post("https://whatsapp.turn.io/v1/messages", body, headers)
   end
 end
 
