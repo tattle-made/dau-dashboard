d96cad722f307fe0d39bca01cd56dfd596ec7c24
maanasb01 <maanasb01@gmail.com>
2026-04-08 12:29:08 +0530

    chore: remove unused library floki

--- mix.exs
+++ mix.exs
@@ -48,7 +48,6 @@ defmodule DAU.MixProject do
       {:phoenix_html, "~> 4.1"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:phoenix_live_view, "== 1.1.0"},
-      {:floki, ">= 0.30.0", only: :test},
       {:lazy_html, ">= 0.0.0", only: :test},
       {:phoenix_live_dashboard, "~> 0.8.3"},
       {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
